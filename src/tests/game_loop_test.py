2a45a67036e510a4c0d112d685c51b4386730d82
Ruben Drayton <ruben.drayton@gmail.com>
2021-12-13 15:48:18 +0200

    added GameLoop testing

---
+++ src/tests/game_loop_test.py
@@ -0,0 +1,19 @@
+import os
+import unittest
+import pygame
+from game_loop import GameLoop
+from level import Level
+from settings import *
+
+
+class TestGameLoop(unittest.TestCase):
+
+    def setUp(self):
+        pygame.init()
+        self.game_loop = GameLoop()
+
+    def test_mouse_pos_is_scaled_correctly(self):
+        self.game_loop.screen = pygame.display.set_mode((2 * SCREEN_WIDTH, 2* SCREEN_HEIGHT))
+        scaled_pos = self.game_loop.scale_mouse((2, 2))
+        print(scaled_pos)
+        self.assertEqual(scaled_pos, [1, 1])
